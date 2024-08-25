6466e27494e04f410bc926352411d1f21794caba
zyng233 <ZziyingTan@gmail.com>
2024-08-26 01:18:48 +0800

    level 0

---
+++ src/main/java/Popi.java
@@ -0,0 +1,8 @@
+public class Popi {
+    public static void main(String[] args) {
+        String newline = "-------------------------";
+        System.out.println(newline + "\nHello! I'm Popi!");
+        System.out.println("What can I do for you?\n" + newline);
+        System.out.println("Bye. Hope to see you again soon!\n" + newline);
+    }
+}
