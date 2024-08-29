27879bff9402486d1f92dc9a9fa209ccb65b2b96
zyng233 <ZziyingTan@gmail.com>
2024-08-30 01:07:57 +0800

    Level 7

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -5,7 +5,18 @@ public class Deadline extends Task {
         this.end = end;
     }
 
+    @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + this.end + ")";
+    }
+
+    @Override
+    protected String getType() {
+        return "D";
+    }
+
+    @Override
+    public String toDataString() {
+        return getType() + " | " + (isDone ? "1" : "0") + " | " + this.description + " | " + this.end;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -7,7 +7,18 @@ public class Event extends Task{
         this.end = end;
     }
 
+    @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + this.start + " to: " + this.end + ")";
+    }
+
+    @Override
+    protected String getType() {
+        return "E";
+    }
+
+    @Override
+    public String toDataString() {
+        return getType() + " | " + (isDone ? "1" : "0") + " | " + description + " | " + start + " | " + end;
     }
 }
